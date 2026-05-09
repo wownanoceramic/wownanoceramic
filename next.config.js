3a164d33b8b72b626a725982ad1d57d22c98285e
termoizolatii-romania <termoizolatiiromania@gmail.com>
2026-05-09 13:59:59 +0300

    Move static files to public, add rewrite

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/', destination: '/index.html' },
+    ]
+  },
+}
 
 module.exports = nextConfig
